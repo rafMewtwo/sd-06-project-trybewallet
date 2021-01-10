67dd40cd9029a9cd472364e77dab1ed1017bc406
rafMewtwo <ponciano.raf@gmail.com>
2021-01-10 18:07:30 -0300

    complet projetc

---
+++ src/pages/Wallet.js
@@ -0,0 +1,16 @@
+/*  eslint linebreak-style: ["error", "windows"]  */
+import React from 'react';
+import Header from '../components/Header';
+import Expenses from '../components/Expenses';
+
+class Wallet extends React.Component {
+  render() {
+    return (
+      <div>
+        <Header />
+        <Expenses />
+      </div>);
+  }
+}
+
+export default Wallet;
---
+++ src/reducers/wallet.js
@@ -0,0 +1,27 @@
+/*  eslint linebreak-style: ["error", "windows"]  */
+// Esse reducer será responsável por tratar o todas as informações relacionadas as despesas
+const INITIAL_STATE = {
+  currencies: [],
+  expenses: [],
+};
+
+export default function reducer(state = INITIAL_STATE, action) {
+  // retorna um novo estado baseado no type da action
+  switch (action.type) {
+  case 'ATT_CURRENCIES':
+    return { ...state, currencies: [...state.currencies, action.currencies] };
+  case 'ATT_EXCHANGERATES':
+    return {
+      ...state,
+      expenses: [...state.expenses,
+        { ...action.expenses,
+          exchangeRates: action.exchangeRates }] };
+  case 'DELL_CURRENCY':
+    return {
+      ...state,
+      expenses: state.expenses.filter((element) => element.id !== action.line),
+    };
+  default:
+    return state;
+  }
+}
